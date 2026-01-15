f2cc1e63175064d4f8de5ce4ad6f39cdf0b791a9
ayappy-mugicha <ayatosync13@gmail.com>
2026-01-15 10:06:10 +0900

    print関数の部分を直したのかな

--- BackEnd/app/workers/mqtt_worker.py
+++ BackEnd/app/workers/mqtt_worker.py
@@ -1,6 +1,7 @@
 from paho.mqtt import client as mqtt_client
 import os
 import sys
+import traceback
 # 1. 自分のいる場所（appフォルダ）の絶対パスを取得
 current_dir = os.path.dirname(os.path.abspath(__file__))
 # 2. 一つ上の階層（親フォルダ）のパスを作る
@@ -15,7 +16,7 @@ import time
 
 # コールバック関数の定義
 
-def on_connect(client,userdata, flags, rc): 
+def on_connect(client,userdata, flags, rc, properties=None): 
     if rc == 0:
         print("接続完了" + str(rc))
         client.subscribe(config.settings.MQTT_TOPIC)
@@ -23,14 +24,14 @@ def on_connect(client,userdata, flags, rc):
     else:
         print("接続失敗" + str(rc))
         
-def on_message(clinet, userdata, msg):
+def on_message(clinet, userdata, msg, properties=None):
     try:
         data = json.loads(msg.payload.decode()) # jsonでMQTTのデータを読み取る
         db = database.SessionLocal() # 新しいセッションを開始
         try:
             # CRUD関数を呼び出しDBにデータを保存
             crud.create_elevator_status(db, data)
-            print(f"DBにデータを保存: EID={data['elevator_id']} 階={data['current_floor']} 人数={data['occupancy']} 方向={data['direction']}")
+            print(f"DBにデータを保存: timestamp={data['timestamp']} EID={data['elevator_id']} 階={data['current_floor']} 人数={data['occupancy']} 方向={data['direction']}")
         
         finally:
             db.close()
@@ -55,6 +56,7 @@ def run_mqtt_worker():
             client.loop_forever() # ブロッキングして常駐プロセスとして実行
         except Exception as e:
             print(f"エラーが発生しました {e}")
+            traceback.print_exc()
             time.sleep(5)
 
 if __name__ == "__main__":
