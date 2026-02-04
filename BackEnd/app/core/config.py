6b6438e79e23d7560491feda8110b897abd0986d
ayappy-mugicha <ayatosync13@gmail.com>
2026-02-04 16:27:57 +0900

    バイナリ形式のフォーマットを追加した

--- BackEnd/app/core/config.py
+++ BackEnd/app/core/config.py
@@ -14,11 +14,13 @@ class Settings(BaseSettings):
     MQTT_HOST: str
     MQTT_PORT: int
     MQTT_TOPIC: str
-
+    FORMAT_STR: str
+    
     model_config = SettingsConfigDict(
         env_file=str(BASE_DIR / ".env"), # .envファイルの場所を指定
         extra='ignore' # 定義されていない環境変数があっても無視する
     )
+    
 
 # 設定をアプリケーション全体で利用できるようにインスタンス化
 settings = Settings()
