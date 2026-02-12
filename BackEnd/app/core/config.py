f105d6a821393792b5ca41efa1a31b08f011ba97
ayappy-mugicha <ayatosync13@gmail.com>
2026-02-12 10:08:03 +0900

    DATABASE_URLを.envファイルではなくpythonで組み立てる方針にした

--- BackEnd/app/core/config.py
+++ BackEnd/app/core/config.py
@@ -21,7 +21,15 @@ class Settings(BaseSettings):
     DB_PORT: int
     DB_NAME: str   
 
-    DATABASE_URL: Optional[str] = None
+    # DATABASE_URL: Optional[str] = None
+    @property
+    def DATABASE_URL(self) -> str:
+        return (
+            f"{self.DB_DRIVER}://"
+            f"{self.DB_USER}:{self.DB_PASSWORD}@"
+            f"{self.DB_HOST}:{self.DB_PORT}/"
+            f"{self.DB_NAME}"
+        )
 
     # MQTT接続情報
     MQTT_HOST: str
@@ -35,16 +43,16 @@ class Settings(BaseSettings):
         extra='ignore' # 定義されていない環境変数があっても無視する
     )
 
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        # DATABASE_URLが明示的に設定されていない場合、自動生成する
-        if not self.DATABASE_URL:
-            self.DATABASE_URL = (
-                f"{self.DB_DRIVER}://"
-                f"{self.DB_USER}:{self.DB_PASSWORD}@"
-                f"{self.DB_HOST}:{self.DB_PORT}/"
-                f"{self.DB_NAME}"
-            )
+    # def __init__(self, **kwargs):
+    #     super().__init__(**kwargs)
+    #     # DATABASE_URLが明示的に設定されていない場合、自動生成する
+    #     if not self.DATABASE_URL:
+    #         self.DATABASE_URL = (
+    #             f"{self.DB_DRIVER}://"
+    #             f"{self.DB_USER}:{self.DB_PASSWORD}@"
+    #             f"{self.DB_HOST}:{self.DB_PORT}/"
+    #             f"{self.DB_NAME}"
+    #         )
 
 # 設定をアプリケーション全体で利用できるようにインスタンス化
 settings = Settings()
